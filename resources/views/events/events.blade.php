67f547cf5b9473ad294d0d589cc2db6a6a06f6e2
Elliott Allmann <elliott.allmann@cox.net>
2019-04-28 19:43:25 -0700

    Add event day separators

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -8,32 +8,122 @@
         </div>
         @endif
         @if($events)
+            <h2> Thursday </h2>
             @foreach($events as $event)
-                <div class="container-fluid d-flex flex-row mb-5 mx-auto">
-                    <div class="container justify-content-center">
-                        <h3>
-                            <a href="/events/{{$event->id}}">{{$event->name}}</a>
-                        </h3>
-                        <p>{{$event->description}}</p>
-                        <p>{{date("l, F d, Y, h:i a", strtotime($event->date))}}</p>
-                        <p>Approx Cost: {{number_format((float) $event->price, 2, '.', '')}}</p>
-                        <a href="{{$event->link}}" target="_blank">{{$event->link}}</a>
-                    </div>
-                    <div class="ml-5 container">
-                        <?php  $found = false; ?>
-                        @foreach($rsvps as $rsvp)
-                            @if($rsvp->eventId == $event->id)
-                                <?php $found = true; ?>
+                @if(date("l", strtotime($event->date)) === "Thursday")
+                    <div class="container-fluid d-flex flex-row mb-5 mx-auto">
+                        <div class="container justify-content-center">
+                            <h3>
+                                <a href="/events/{{$event->id}}">{{$event->name}}</a>
+                            </h3>
+                            <p>{{$event->description}}</p>
+                            <p>{{date("l, F d, Y, h:i a", strtotime($event->date))}}</p>
+                            <p>Approx Cost: {{number_format((float) $event->price, 2, '.', '')}}</p>
+                            <a href="{{$event->link}}" target="_blank">{{$event->link}}</a>
+                        </div>
+                        <div class="ml-5 container">
+                            <?php  $found = false; ?>
+                            @foreach($rsvps as $rsvp)
+                                @if($rsvp->eventId == $event->id)
+                                    <?php $found = true; ?>
+                                @endif
+                            @endforeach
+                            @if($found)
+                                <button type="button" class="btn btn-success">Registered!</button>
+                            @else
+                                <button type="button" class="btn btn-primary" data-id="{{$event->id}}" id="rsvpModalLaunch" data-toggle="modal" data-target="#rsvpModal">RSVP</button>
                             @endif
-                        @endforeach
-                        @if($found)
-                            <button type="button" class="btn btn-success">Registered!</button>
-                        @else
-                            <button type="button" class="btn btn-primary" data-id="{{$event->id}}" id="rsvpModalLaunch" data-toggle="modal" data-target="#rsvpModal">RSVP</button>
-                        @endif
+                        </div>
                     </div>
-                </div>
+                    @endif
             @endforeach
+            <h2> Friday </h2>
+            @foreach($events as $event)
+                @if(date("l", strtotime($event->date)) === "Friday")
+                    <div class="container-fluid d-flex flex-row mb-5 mx-auto">
+                        <div class="container justify-content-center">
+                            <h3>
+                                <a href="/events/{{$event->id}}">{{$event->name}}</a>
+                            </h3>
+                            <p>{{$event->description}}</p>
+                            <p>{{date("l, F d, Y, h:i a", strtotime($event->date))}}</p>
+                            <p>Approx Cost: {{number_format((float) $event->price, 2, '.', '')}}</p>
+                            <a href="{{$event->link}}" target="_blank">{{$event->link}}</a>
+                        </div>
+                        <div class="ml-5 container">
+                            <?php  $found = false; ?>
+                            @foreach($rsvps as $rsvp)
+                                @if($rsvp->eventId == $event->id)
+                                    <?php $found = true; ?>
+                                @endif
+                            @endforeach
+                            @if($found)
+                                <button type="button" class="btn btn-success">Registered!</button>
+                            @else
+                                <button type="button" class="btn btn-primary" data-id="{{$event->id}}" id="rsvpModalLaunch" data-toggle="modal" data-target="#rsvpModal">RSVP</button>
+                            @endif
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+            <h2> Saturday </h2>
+            @foreach($events as $event)
+                @if(date("l", strtotime($event->date)) === "Saturday")
+                    <div class="container-fluid d-flex flex-row mb-5 mx-auto">
+                        <div class="container justify-content-center">
+                            <h3>
+                                <a href="/events/{{$event->id}}">{{$event->name}}</a>
+                            </h3>
+                            <p>{{$event->description}}</p>
+                            <p>{{date("l, F d, Y, h:i a", strtotime($event->date))}}</p>
+                            <p>Approx Cost: {{number_format((float) $event->price, 2, '.', '')}}</p>
+                            <a href="{{$event->link}}" target="_blank">{{$event->link}}</a>
+                        </div>
+                        <div class="ml-5 container">
+                            <?php  $found = false; ?>
+                            @foreach($rsvps as $rsvp)
+                                @if($rsvp->eventId == $event->id)
+                                    <?php $found = true; ?>
+                                @endif
+                            @endforeach
+                            @if($found)
+                                <button type="button" class="btn btn-success">Registered!</button>
+                            @else
+                                <button type="button" class="btn btn-primary" data-id="{{$event->id}}" id="rsvpModalLaunch" data-toggle="modal" data-target="#rsvpModal">RSVP</button>
+                            @endif
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+            <h2> Sunday </h2>
+            @foreach($events as $event)
+                @if(date("l", strtotime($event->date)) === "Sunday")
+                    <div class="container-fluid d-flex flex-row mb-5 mx-auto">
+                        <div class="container justify-content-center">
+                            <h3>
+                                <a href="/events/{{$event->id}}">{{$event->name}}</a>
+                            </h3>
+                            <p>{{$event->description}}</p>
+                            <p>{{date("l, F d, Y, h:i a", strtotime($event->date))}}</p>
+                            <p>Approx Cost: {{number_format((float) $event->price, 2, '.', '')}}</p>
+                            <a href="{{$event->link}}" target="_blank">{{$event->link}}</a>
+                        </div>
+                        <div class="ml-5 container">
+                            <?php  $found = false; ?>
+                            @foreach($rsvps as $rsvp)
+                                @if($rsvp->eventId == $event->id)
+                                    <?php $found = true; ?>
+                                @endif
+                            @endforeach
+                            @if($found)
+                                <button type="button" class="btn btn-success">Registered!</button>
+                            @else
+                                <button type="button" class="btn btn-primary" data-id="{{$event->id}}" id="rsvpModalLaunch" data-toggle="modal" data-target="#rsvpModal">RSVP</button>
+                            @endif
+                        </div>
+                    </div>
+            @endif
+        @endforeach
         @endif
         <!-- Modal -->
         <div class="modal fade" id="rsvpModal" tabindex="-1" role="dialog" aria-labelledby="rsvpModalLabel" aria-hidden="true">
